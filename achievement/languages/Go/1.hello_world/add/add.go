26e3af8e995238cb9657e803612b82b8dff51cbe
Nishida Ryousuke <55966582+24daryo@users.noreply.github.com>
2022-04-03 12:15:16 +0900

    write readme

--- achievement/languages/Go/1.hello_world/add/add.go
+++
@@ -1,7 +0,0 @@
-package add
-
-// Note that if Add has the same name as add, 
-//it cannot be referenced from the outside.
-func Add(a, b int) int {
-    return a + b
-}
--- achievement/languages/Go/1.hello_world/go.mod
+++
@@ -1,7 +0,0 @@
-module hello_world
-
-go 1.16
-
-replace 1.hello_world/add => ./add
-
-require 1.hello_world/add v0.0.0-00010101000000-000000000000 // indirect
